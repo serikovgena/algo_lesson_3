b67ef736c625a9d55de5d45186fd762a7f630b13
GennadiySerikov <serikov.gen@yandex.ru>
2021-10-12 16:12:14 +0300

    feat: add custom time logger

--- lesson3/Program.cs
+++ lesson3/Program.cs
@@ -1,11 +1,56 @@
 ﻿using System;
+using System.Diagnostics;
 
 namespace lesson3
 {
     class Program
     {
-        static void Main(string[] args) {
-            // using BenchmartDotNet for performance testing
+        static void Main() {
+          TestDistancer();
+        }
+
+        static void TestDistancer() {
+          PointClass startPointClassFloat = new() { x = 1, y = 1 };
+          PointClass endPointClassFloat = new() { x = 5, y = -2 };
+          
+          PointStructFloat startPointStructFloat = new() { x = 1, y = 1 };
+          PointStructFloat endPointStructFloat = new() { x = 5, y = -2 };
+          
+          PointStructDouble startPointStructDouble = new() { x = 1, y = 1 };
+          PointStructDouble endPointStructDouble = new() { x = 5, y = -2 };
+          
+          Distancer distancer = new();
+          
+          using (new TimeLogger())
+          {
+              _ = distancer.MeasureDistance(startPointClassFloat, endPointClassFloat);
+          }
+          using (new TimeLogger())
+          {
+              _ = distancer.MeasureDistance(startPointStructDouble, endPointStructDouble);
+          }
+          using (new TimeLogger())
+          {
+              _ = distancer.MeasureDistanceBySin(startPointStructFloat, endPointStructFloat);
+          }
+          using (new TimeLogger())
+          {
+              _ = distancer.MeasureDistanceBySin(startPointStructFloat, endPointStructFloat);
+          }
+       }
+    }
+
+    class TimeLogger : IDisposable
+    {
+        Stopwatch timer;
+        public TimeLogger() {
+            timer = new Stopwatch();
+            timer.Start();
+        }
+
+        public void Dispose() {
+            timer.Stop();
+            Console.WriteLine($"{timer.Elapsed.TotalMilliseconds} ms");
         }
     }
 }
